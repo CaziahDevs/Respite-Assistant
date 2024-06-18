e13ebf86ae85d705b0f528e76185cc207d6d27fa
Cazi <71891696+Cazi@users.noreply.github.com>
2024-06-17 21:18:59 -0400

    Updated the url, so that login functionality is unnecessary

--- src/main.py
+++ src/main.py
@@ -11,49 +11,9 @@ import time
 # Load environment variables from a.env file
 env_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(env_path)
-username = os.getenv('username')
-password = os.getenv('password')
 url = os.getenv('url')
 driver = webdriver.Chrome()
 
-def login() -> None:
-    """
-    This function is used to login to the company's website.
-
-    Parameters:
-    driver (WebDriver): The WebDriver instance used to interact with the page.
-
-    Returns:
-    None
-
-    """
-    driver.get(url)
-    WebDriverWait(driver, 5)
-    # Add code to fill in username and password fields
-    username_input = driver.find_element(By.NAME, "lo_uname")
-    username_input.send_keys(username)
-    password_input = driver.find_element(By.NAME, "lo_pw")
-    password_input.send_keys(password, Keys.ENTER)
-    WebDriverWait(driver, 5)
-
-def naviagate_to_clock() -> None:
-    """
-    This function is used to navigate to the clock in/out page.
-
-    Parameters:
-    driver (WebDriver): The WebDriver instance used to interact with the page.
-
-    Returns:
-    None
-
-    """
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located(
-            (By.PARTIAL_LINK_TEXT, "Clock In/Out"))
-    )
-    # Click on the "Clock In" button
-    link = driver.find_element(By.PARTIAL_LINK_TEXT, "Clock In/Out")
-    link.click()
 
 def clock_in() -> None:
     """
@@ -104,15 +64,22 @@ def main() -> None:
     current_time = time.localtime()
     clock_in_condition = (current_time.tm_wday < 5) and (current_time.tm_hour == 18) and (current_time.tm_min == 0)
     clock_out_condition = (current_time.tm_wday == 4) and (current_time.tm_hour == 21) and (current_time.tm_min == 0)
-    login()
-    naviagate_to_clock()
-    if clock_in_condition:
-        clock_in()
-    elif clock_out_condition:
-        clock_out()
-    else:
-        print("No changes made to the timesheet")
-    driver.close()
+
+    try:
+        driver.get(url)
+        WebDriverWait(driver, 20)
+        print("loading page...", url)
+        if clock_in_condition:
+            clock_in()
+        elif clock_out_condition:
+            clock_out()
+        else:
+            print("No changes made to the timesheet")
+        driver.close()
+    
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
     main()
+
